0ed8b632c6b47b11d97284eb2e480c5af0bd0d06
Iljas-Leys <r0803440@student.thomasmore.be>
2026-01-12 12:06:06 +0100

    fix for python311

--- runLocalDev.ps1
+++ runLocalDev.ps1
@@ -1,4 +1,4 @@
 . .\.venv\Scripts\Activate.ps1
 
 # Run MCP server
-Start-Process python -ArgumentList ".\AI\mcpServer.py"
+Start-Process .venv\Scripts\python.exe -ArgumentList ".\AI\mcpServer.py"
--- setupLocalDev.ps1
+++ setupLocalDev.ps1
@@ -1,14 +1,13 @@
-:: -d for detached -> runs in background and doesn't stay open in terminal
+# -d for detached -> runs in background and doesn't stay open in terminal
 docker run -d --name skill-project-redisvl -p 6379:6379 -it redis/redis-stack-server
 docker run -d --name skill-project-postgres -e POSTGRES_PASSWORD=postgres -e POSTGRES_USER=postgres -e POSTGRES_DB=skilldb -p 5432:5432 postgres:16
 
-IF NOT EXIST .venv (
+if (-not (Test-Path -Path ".venv")) {
     py -3.11 -m venv .venv
-)
-CALL .venv\Scripts\activate
-python -m pip install -r pythonRequirements.txt
+}
+& .venv\Scripts\python.exe -m pip install -r pythonRequirements.txt
 
-python setupRedis.py
-python setupPostgress.py
+& .venv\Scripts\python.exe setupRedis.py
+& .venv\Scripts\python.exe setupPostgress.py
 
 pause
