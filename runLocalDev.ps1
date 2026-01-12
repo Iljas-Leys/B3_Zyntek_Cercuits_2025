39826d853cddc07b4c973d87cebee2560c713df2
Iljas-Leys <r0803440@student.thomasmore.be>
2026-01-12 11:12:52 +0100

    MCP server, postgress setup

---
+++ runLocalDev.ps1
@@ -0,0 +1,4 @@
+. .\.venv\Scripts\Activate.ps1
+
+# Run MCP server
+Start-Process python -ArgumentList ".\AI\mcpServer.py"
--- setupLocalDev.bat
+++ setupLocalDev.bat
@@ -1,13 +1,14 @@
 :: -d for detached -> runs in background and doesn't stay open in terminal
 docker run -d --name skill-project-redisvl -p 6379:6379 -it redis/redis-stack-server
-docker run -d --name skill-project-postgres -e POSTGRES_PASSWORD=postgres -e POSTGRES_USER=postgres -e POSTGRES_DB=mydb -p 5432:5432 postgres:16
+docker run -d --name skill-project-postgres -e POSTGRES_PASSWORD=postgres -e POSTGRES_USER=postgres -e POSTGRES_DB=skilldb -p 5432:5432 postgres:16
 
 IF NOT EXIST .venv (
-    python -m venv .venv
+    py -3.11 -m venv .venv
 )
 CALL .venv\Scripts\activate
 python -m pip install -r pythonRequirements.txt
 
 python setupRedis.py
+python setupPostgress.py
 
 pause
