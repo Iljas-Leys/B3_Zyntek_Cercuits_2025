58a088d79578b91c0c1547139554ed63d7f5bfdf
Iljas-Leys <r0803440@student.thomasmore.be>
2026-01-13 10:55:48 +0100

    small fix

--- runLocalDev.ps1
+++ runLocalDev.ps1
@@ -1,9 +1,9 @@
 . .\.venv\Scripts\Activate.ps1
 
 # Run MCP server
-Start-Process powershell -ArgumentList"-NoExit", "-Command", "& .\.venv\Scripts\python.exe AI/mcpServer.py"
+#Start-Process powershell -ArgumentList"-NoExit", "-Command", "& .\.venv\Scripts\python.exe AI/mcpServer.py"
 
 # Run backend
-#Start-Process powershell -ArgumentList "-NoExit", "-Command", "cd APP/backend; & ../../.venv/Scripts/python.exe -m uvicorn app.main:app --reload"
+Start-Process powershell -ArgumentList "-NoExit", "-Command", "cd APP/backend; & ../../.venv/Scripts/python.exe -m uvicorn app.main:app --reload"
 
 pause
